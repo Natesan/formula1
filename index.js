8b51e5e23750cf9aa1e481efc8639d35b2c27bd2
natesan.sivagnanam <natesan.sivagnanam@sap.com>
2019-08-10 17:36:13 +0530

    Minor fixes to address UX

--- index.js
+++ index.js
@@ -1,63 +1,70 @@
 jQuery(document).ready(function () {
-    console.log("Lights Out! And away we go!");
+    jQuery("body").on('click', function () {
+        console.log("Lights Out! And away we go!");
+        var mercedez = document.getElementById('mercedez');
+        var ferrari = document.getElementById('ferrari');
+        var redbull = document.getElementById('redbull');
 
-    var mercedez = document.getElementById('mercedez');
-    mercedez.addEventListener("animationstart", listener, false);
-    mercedez.addEventListener("animationend", listener, false);
-    mercedez.addEventListener("animationiteration", listener, false);
+        // Setup for a standard Free Practice 1
+        mercedez.className = "fp1 standard-session";
+        ferrari.className = "fp1 standard-session";
+        redbull.className = "fp1 standard-session";
 
-    var ferrari = document.getElementById('ferrari');
-    ferrari.addEventListener("animationstart", listener, false);
-    ferrari.addEventListener("animationend", listener, false);
-    ferrari.addEventListener("animationiteration", listener, false);
+        // Team and its strategies which plat a key role across sessions decided here
+        mercedez.addEventListener("animationstart", listener, false);
+        mercedez.addEventListener("animationend", listener, false);
+        mercedez.addEventListener("animationiteration", listener, false);
 
-    var redbull = document.getElementById('redbull');
-    redbull.addEventListener("animationstart", listener, false);
-    redbull.addEventListener("animationend", listener, false);
-    redbull.addEventListener("animationiteration", listener, false);
+        ferrari.addEventListener("animationstart", listener, false);
+        ferrari.addEventListener("animationend", listener, false);
+        ferrari.addEventListener("animationiteration", listener, false);
 
-    function listener(oEvent) {
-        switch (oEvent.type) {
-            case "animationstart":
-                console.log(oEvent.target.className + ' Starts');
-                break;
-            case "animationend":
-                console.log(oEvent.target.className + ' Ends');
-                if (oEvent.target.className.includes("fp1")) { // entering fp2
-                    if (oEvent.target.id == "ferrari") {
-                        oEvent.target.className = "fp2 mega-session";
-                    } else {
-                        oEvent.target.className = "fp2 standard-session";
+        redbull.addEventListener("animationstart", listener, false);
+        redbull.addEventListener("animationend", listener, false);
+        redbull.addEventListener("animationiteration", listener, false);
+
+        function listener(oEvent) {
+            switch (oEvent.type) {
+                case "animationstart":
+                    console.log(oEvent.target.className + ' Starts');
+                    break;
+                case "animationend":
+                    console.log(oEvent.target.className + ' Ends');
+                    if (oEvent.target.className.includes("fp1")) { // Teams entering FP2
+                        if (oEvent.target.id == "ferrari") {
+                            oEvent.target.className = "fp2 mega-session";
+                        } else {
+                            oEvent.target.className = "fp2 standard-session";
+                        }
+                    } else if (oEvent.target.className.includes("fp2")) { // Teams entering FP3
+                        if (oEvent.target.id == "ferrari") {
+                            oEvent.target.className = "fp3 slow-session";
+                        } else if (oEvent.target.id == "mercedez") {
+                            oEvent.target.className = "fp3 mega-session";
+                        } else if (oEvent.target.id == "redbull") {
+                            oEvent.target.className = "fp3 mega-session";
+                        }
+                    } else if (oEvent.target.className.includes("fp3")) { // Teams entering Qualification
+                        if (oEvent.target.id == "ferrari") {
+                            oEvent.target.className = "quali no-session";
+                        } else if (oEvent.target.id == "mercedez") {
+                            oEvent.target.className = "quali standard-session";
+                        } else if (oEvent.target.id == "redbull") {
+                            oEvent.target.className = "quali slow-session";
+                        }
+                    } else if (oEvent.target.className.includes("quali")) { // Teams entering Race Day
+                        if (oEvent.target.id == "ferrari") {
+                            oEvent.target.className = "race ultraslow-session";
+                        } else if (oEvent.target.id == "mercedez") {
+                            oEvent.target.className = "race standard-session";
+                        } else if (oEvent.target.id == "redbull") {
+                            oEvent.target.className = "race slow-session";
+                        }
+                    } else if (oEvent.target.className.includes("race")) {
+                        oEvent.target.className = "done";
                     }
-                } else if (oEvent.target.className.includes("fp2")) { // entering fp3
-                    if (oEvent.target.id == "ferrari") {
-                        oEvent.target.className = "fp3 slow-session";
-                    } else if (oEvent.target.id == "mercedez") {
-                        oEvent.target.className = "fp3 mega-session";
-                    } else if (oEvent.target.id == "redbull") {
-                        oEvent.target.className = "fp3 mega-session";
-                    }
-                } else if (oEvent.target.className.includes("fp3")) { // entering quali
-                    if (oEvent.target.id == "ferrari") {
-                        oEvent.target.className = "quali no-session";
-                    } else if (oEvent.target.id == "mercedez") {
-                        oEvent.target.className = "quali standard-session";
-                    } else if (oEvent.target.id == "redbull") {
-                        oEvent.target.className = "quali slow-session";
-                    }
-                } else if (oEvent.target.className.includes("quali")) { // entering race
-                    if (oEvent.target.id == "ferrari") {
-                        oEvent.target.className = "race ultraslow-session";
-                    } else if (oEvent.target.id == "mercedez") {
-                        oEvent.target.className = "race standard-session";
-                    } else if (oEvent.target.id == "redbull") {
-                        oEvent.target.className = "race slow-session";
-                    }
-                } else if (oEvent.target.className.includes("race")) {
-                    oEvent.target.className = "done";
-                }
-                break;
+                    break;
+            }
         }
-    }
-
+    });
 });
